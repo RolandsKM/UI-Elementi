e8352ed5bb19bab85e5f4d6e334c9bc62798b925
Makarovs <rolandsmakarovs264@gmail.com>
2023-04-19 12:20:55 +0300

    Tikt tik tālu

---
+++ Assets/skripti/VarduParadisana.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VarduParadisana : MonoBehaviour {
+
+	int kadruSk = 0;
+
+
+
+	void Start () {
+		Debug.Log("Uzsākta Programa! Tu esi "+kadruSk+" kadrā");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("Kadrs: " + kadruSk );
+		kadruSk++;
+
+    }
+}
