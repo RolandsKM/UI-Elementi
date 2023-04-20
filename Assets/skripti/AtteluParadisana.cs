dd84b0aca106cf768dd17fe037ccc5a0ad00f7d2
Makarovs <rolandsmakarovs264@gmail.com>
2023-04-20 12:16:08 +0300

    Nav isti pabeikts

---
+++ Assets/skripti/AtteluParadisana.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AtteluParadisana : MonoBehaviour {
+
+	public GameObject binsImg;
+	public GameObject lacitisImg;
+	public GameObject omeImg;
+	public GameObject masinaImg;
+    public GameObject PaKreisiPoga;
+    public GameObject PaLabiPoga;
+    public GameObject mainigaisAttels;
+    public Sprite[] AtteluMasivs;
+
+    public void BinaAtelosana(bool vertiba)
+	{
+		binsImg.SetActive(vertiba);
+        PaKreisiPoga.GetComponent<Toggle>().interactable = vertiba;
+        PaLabiPoga.GetComponent<Toggle>().interactable = vertiba;
+
+    }
+    public void Lacattelosana(bool vertiba)
+    {
+        lacitisImg.SetActive(vertiba);
+
+    }
+    public void Omeattelosana(bool vertiba)
+    {
+        omeImg.SetActive(vertiba);
+
+    }
+    public void Masinaattelosana(bool vertiba)
+    {
+        masinaImg.SetActive(vertiba);
+
+    }
+    public void PaKreisiBeans()
+    {
+        binsImg.transform.localScale = new Vector2(1, 1);
+
+    }
+    public void PaLabiBeans()
+    {
+        binsImg.transform.localScale = new Vector2(-1, 1);
+
+    }
+    public void izkritosais(int skaitlis)
+    {
+        if (skaitlis == 0)
+
+            mainigaisAttels.GetComponent<Image>().sprite = AtteluMasivs[0];
+        else if (skaitlis == 1)
+            mainigaisAttels.GetComponent<Image>().sprite = AtteluMasivs[1];
+        else if (skaitlis == 2)
+            mainigaisAttels.GetComponent<Image>().sprite = AtteluMasivs[2];
+        else
+            Debug.Log("Nev piesaistītais attels");
+    }
+}
--- Assets/skripti/VarduParadisana.cs
+++ Assets/skripti/VarduParadisana.cs
@@ -1,21 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VarduParadisana : MonoBehaviour {
+	public string teksts;
+	public GameObject IevadesLauks;
+	public GameObject TekstaAttelosana;
+	private string[] fragmenti = {"Sveiks", "JaukuDienu", "PrieksTevi Redzēt", "Sup"};
+	int indekss;
+	public void UzglabaTekstu()
+	{
+		indekss = Random.Range(0, fragmenti.Length);
+		teksts = IevadesLauks.GetComponent<InputField>().text;
+		TekstaAttelosana.GetComponent<Text>().text = fragmenti[indekss] + teksts.ToUpper() + "!";
 
-	int kadruSk = 0;
-
-
-
-	void Start () {
-		Debug.Log("Uzsākta Programa! Tu esi "+kadruSk+" kadrā");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		Debug.Log("Kadrs: " + kadruSk );
-		kadruSk++;
 
     }
+
 }
